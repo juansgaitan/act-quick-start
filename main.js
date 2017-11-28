4b61ad94bff9929c3564611fe61a8311dfee0172
Juan Sebastián Gaitán Villamizar <juans.gaitan@gmail.com>
2017-11-28 12:52:34 +0100

    Added comment to type-check good practice.

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 const Apify = require('apify');
-const typeCheck = require('type-check').typeCheck;
+const { typeCheck } = require('type-check');
 
-// Definition of the input
+// Definition of the input. Good practice.
 const INPUT_TYPE = `{
     message: Maybe String,
 }`;
 
 Apify.main(async () => {
-    // Fetch the input and check it has a valid format
-    // You don't need to check the input, but it's a good practice.
-    const input = await Apify.getValue('INPUT');
-    if (!typeCheck(INPUT_TYPE, input)) {
-        console.log('Expected input:');
-        console.log(INPUT_TYPE);
-        console.log('Received input:');
-        console.dir(input);
-        throw new Error('Received invalid input');
-    }
+  // Fetch the input and check it has a valid format
+  // You don't need to check the input, but it's a good practice.
+  const input = await Apify.getValue('INPUT');
+  if (!typeCheck(INPUT_TYPE, input)) {
+    console.log('Expected input:');
+    console.log(INPUT_TYPE);
+    console.log('Received input:');
+    console.dir(input);
+    throw new Error('Received invalid input');
+  }
 
-    // Here's the place for your magic...
-    console.log(`Input message: ${input.message}`);
+  // Here's the place for your magic...
+  console.log(`Input message: ${input.message}`);
 
-    // Store the output
-    const output = {
-        message: `${input.message} Hello my friend!`
-    };
-    await Apify.setValue('OUTPUT', output)
+  // Store the output
+  const output = {
+    message: `${input.message} Hello my friend!`,
+  };
+  await Apify.setValue('OUTPUT', output);
 });
